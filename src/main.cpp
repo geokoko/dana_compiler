b7a3ddb9e9fd266de6a79bc44c20b25dfb635842
geokoko <geokoko3@outlook.com.gr>
2025-12-11 12:18:06 +0200

    Update main.cpp to support full compilation pipeline
    Also add llvm module verifier to catch incorrect IR cases

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,32 @@
 #include "./frontend/semantic/diagnostics.hpp"
 #include "./frontend/semantic/sema_context.hpp"
 #include "./frontend/symbol/symbol_table.hpp"
+#include "./backend/codegen/codegen_context.hpp"
+#include "./backend/codegen/codegen.hpp"
+#include "./runtime/danalib.hpp"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/IR/Verifier.h"
 
 extern FILE* yyin;
+
+static std::string change_extension(const std::string& path, const std::string& newExt) {
+    std::size_t slashPos = path.find_last_of("/\\");
+    std::size_t dotPos   = path.find_last_of('.');
+    if (dotPos == std::string::npos || (slashPos != std::string::npos && dotPos < slashPos)) {
+        return path + newExt;
+    }
+    return path.substr(0, dotPos) + newExt;
+}
+
+static bool run_command(const std::string& cmd) {
+    int rc = std::system(cmd.c_str());
+    if (rc != 0) {
+        std::cerr << "Command failed (" << rc << "): " << cmd << "\n";
+        return false;
+    }
+    return true;
+}
 
 static void print_usage(const char* prog) {
     std::cerr << "Usage: " << prog << " [--ast-tree] <file.dana>\n";
@@ -37,11 +61,10 @@ int main(int argc, char** argv) {
 
     yyin = std::fopen(fname, "r");
     if (!yyin) {
-        std::cerr << "Error: could not open file " << fname << "\n";
+        std::cerr << "Error: could not open file " << fname << ".\n";
         return 1;
     }
 
-    // to show location of errors
     dana::parser::location_type loc;
 
     std::string filename = fname;
@@ -49,7 +72,6 @@ int main(int argc, char** argv) {
     loc.begin.filename = &filename;
     loc.end.filename = &filename;
 
-    // Parse and collect AST root via parser's parse-param
     up<Program> ast_root;
     dana::parser parser(ast_root);
     int res = parser.parse();
@@ -57,12 +79,12 @@ int main(int argc, char** argv) {
     std::fclose(yyin);
 
     if (res != 0) {
-        std::cerr << "Parsing finished: failure (parse() returned " << res << ")\n";
+        std::cerr << "Parsing finished: failure (parse() returned " << res << ").\n";
         return res;
     }
 
     if (!ast_root) {
-        std::cerr << "Parsing produced no AST\n";
+        std::cerr << "Parsing produced no AST.\n";
         return 1;
     }
 
@@ -73,13 +95,74 @@ int main(int argc, char** argv) {
     SymbolTable symtab;
     Diagnostics diags;
     SemContext semCtx(symtab, diags);
+    declareBuiltins(semCtx);
 
     ast_root->sem(semCtx);
 
     if (semCtx.hasErrors()) {
         semCtx.printDiagnostics();
+        std::cerr << "Semantic analysis finished with errors.\n";
         return 1;
     }
 
+    CodegenContext codegenCtx("dana_module");
+    genBuiltins(semCtx, codegenCtx);
+    LLVMCodegen generate(codegenCtx);
+    ast_root->agen(generate);
+
+	if (llvm::verifyModule(codegenCtx.llvmModule(), &llvm::errs())) {
+    	std::cerr << "Error: generated LLVM IR is invalid.\n";
+    	return 1;
+	}
+
+    codegenCtx.llvmModule().print(llvm::outs(), nullptr);
+	llvm::outs().flush();
+
+	std::string immPath = change_extension(filename, ".imm");
+    {
+        std::error_code EC;
+        llvm::raw_fd_ostream immOut(immPath, EC, llvm::sys::fs::OF_Text);
+        if (EC) {
+            std::cerr << "Error: could not open " << immPath << " for writing: "
+                      << EC.message() << "\n";
+            return 1;
+        }
+        codegenCtx.llvmModule().print(immOut, nullptr);
+    }
+    std::string asmPath = change_extension(filename, ".asm");
+    {
+        std::string cmd = "llc -filetype=asm \"" + immPath + "\" -o \"" + asmPath + "\"";
+        if (!run_command(cmd)) {
+            std::cerr << "Failed to generate assembly (.asm).\n";
+            return 1;
+        }
+    }
+
+    std::string objPath = change_extension(filename, ".o");
+    {
+        std::string cmd = "llc -filetype=obj \"" + immPath + "\" -o \"" + objPath + "\"";
+        if (!run_command(cmd)) {
+            std::cerr << "Failed to generate object file (.o).\n";
+            return 1;
+        }
+    }
+
+    std::string execPath = "a.out";
+    {
+        std::string linkCmd = "gcc \"" + objPath + "\" runtime.o -o \"" + execPath + "\"";
+        if (!run_command(linkCmd)) {
+            std::cerr << "Failed to link executable.\n";
+            return 1;
+        }
+    }
+
+    std::cout << "Done.\n";
+    std::cout << "Generated:\n"
+              << "  Intermediate: " << immPath << "\n"
+              << "  Assembly:     " << asmPath << "\n"
+              << "  Object:       " << objPath << "\n"
+              << "  Executable:   ./a.out\n";
+
     return 0;
 }
+
