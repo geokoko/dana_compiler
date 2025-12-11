535292d3bbacefe8e73032894d16f5f581100b07
geokoko <geokoko3@outlook.com.gr>
2025-12-11 12:15:46 +0200

    Add llvm::Function and FuncSymbol binding for built-ins
    This complies with the standard methodology of registering any function for codegen to the context

--- src/runtime/danalib.hpp
+++ src/runtime/danalib.hpp
@@ -1,21 +1,21 @@
 #pragma once
 
 struct DanaBuiltin {
-    const char* runtimeName;
-    const char* danaName;
+	const char* runtimeName;
+	const char* danaName;
 };
 
 inline constexpr DanaBuiltin builtinTable[] = {
-    {"dana_writeInteger", "writeInteger"},
-    {"dana_writeByte", "writeByte"},
-    {"dana_writeChar", "writeChar"},
-    {"dana_writeString", "writeString"},
-    {"dana_readInteger", "readInteger"},
-    {"dana_readByte", "readByte"},
-    {"dana_readChar", "readChar"},
-    {"dana_readString", "readString"},
-    {"dana_extend", "extend"},
-    {"dana_shrink", "shrink"},
+	{"dana_writeInteger", "writeInteger"},
+	{"dana_writeByte", "writeByte"},
+	{"dana_writeChar", "writeChar"},
+	{"dana_writeString", "writeString"},
+	{"dana_readInteger", "readInteger"},
+	{"dana_readByte", "readByte"},
+	{"dana_readChar", "readChar"},
+	{"dana_readString", "readString"},
+	{"dana_extend", "extend"},
+	{"dana_shrink", "shrink"},
 	{"dana_strlen", "strlen"},
 	{"dana_strcpy", "strcpy"},
 	{"dana_strcat", "strcat"},
@@ -25,5 +25,5 @@ inline constexpr DanaBuiltin builtinTable[] = {
 class CodegenContext;
 class SemContext;
 
-static void declareBuiltins(SemContext& semCtx);
-static void genBuiltins(CodegenContext& codegenCtx);
+void declareBuiltins(SemContext& semCtx);
+void genBuiltins(SemContext& semCtx, CodegenContext& codegenCtx);
